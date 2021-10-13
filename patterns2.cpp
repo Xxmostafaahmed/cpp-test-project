a135204bb277dc238368c3410fd315dd2e76e388
Xxmostafaahmed <92220702+Xxmostafaahmed@users.noreply.github.com>
2021-10-13 21:54:57 +0530

    Add files via upload
    pattern problem

---
+++ patterns2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int n;
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    {
+        for (int j=1;j<=n-1;j++)
+            if (j<=n-1)
+        {
+
+        }
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+return 0;
+}
