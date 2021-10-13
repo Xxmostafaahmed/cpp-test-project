579c5f4e14481aaa663b3a1ec9c51d11b30958d1
Xxmostafaahmed <92220702+Xxmostafaahmed@users.noreply.github.com>
2021-10-13 21:59:17 +0530

    Add files via upload
    p p

---
+++ patterns5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int n;
+    cin>>n;
+    for (int i=1,k=0;i<=n;i++,k=0)
+    {
+        for (int s=1;s<=(n-i);s++)
+        {
+            cout<<" ";
+        }
+        while(k!=(2*i-1))
+            //jb tak k 0 nahi hota tab tak k ki value looo
+        {
+            cout<<"*";
+            k++;
+        }
+        cout<<endl;
+    }
+
+    for (int i=n-1,k=0;i>=1;i--,k=0)
+    {
+        for (int s=1;s<=(n-i);s++)
+        {
+            cout<<" ";
+        }
+        while(k!=(2*i-1))
+            //jb tak k 0 nahi hota tab tak k ki value looo
+        {
+            cout<<"*";
+            k++;
+        }
+        cout<<endl;
+    }
+
+
+
+return 0;
+}
+
+
